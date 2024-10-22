fb481e78f511aef589b9260884862e38efa73d15
lmoraesdev <lbc_moraes@outlook.com>
2024-10-22 13:00:55 -0300

    feat: novos tester no add e validation e error

--- src/adapters/controllers/task/addTask.spec.ts
+++ src/adapters/controllers/task/addTask.spec.ts
@@ -1,24 +1,18 @@
-import {
-  DbAddTask,
-  MongoManager,
-  TaskMongoRepository,
-} from "../../../dataSources";
 import { Task } from "../../../entities/task";
 import { AddTask, AddTaskModel } from "../../../usecases";
-import { addTaskValidationCompositeFactory } from "../../factories";
 import { HttpRequest, Validation } from "../../interfaces";
-import env from "../../presentations/api/config/env";
+import { serverError } from "../../presentations/api/httpResponses/httpResponses";
 import { AddTaskController } from "./addTask";
 
 const makeAddTask = (): AddTask => {
   class AddTaskStub implements AddTask {
     async add(task: AddTaskModel): Promise<Task> {
-      return {
+      return Promise.resolve({
         id: "any_id",
         title: "any_title",
         description: "any_description",
         date: "30/06/2024",
-      };
+      });
     }
   }
   return new AddTaskStub();
@@ -73,5 +67,28 @@ describe("AddTask Controller", () => {
       description: "any_description",
       date: "30/06/2024",
     });
+  });
+
+  test("Deve retornar 500 se AddTask lançar uma exceção", async () => {
+    const { sut, addTaskStub } = makeSut();
+
+    jest
+      .spyOn(addTaskStub, "add")
+      .mockImplementationOnce(async () => Promise.reject(new Error()));
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test("Deve Chamar Validation com valores corretos", async () => {
+    const { sut, validationStub } = makeSut();
+    const validationSpy = jest.spyOn(validationStub, "validate");
+    const httpRequest = makeFakeRequest();
+
+    await sut.handle(httpRequest);
+
+    expect(validationSpy).toHaveBeenCalledWith(httpRequest.body);
+
+    expect;
   });
 });
